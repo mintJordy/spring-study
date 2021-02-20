c3f3a066477fa371988590c75a3ade8cd529c077
mintJordy <stj930@gmail.com>
2021-02-21 01:02:35 +0900

    [STUDY-spring-study-1]: paging을 고려한 JpaPagingItemReader 추가

--- spring-batch-getting-start/src/main/java/com/taejin/study/jobs/InactiveUserJobConfig.java
+++ spring-batch-getting-start/src/main/java/com/taejin/study/jobs/InactiveUserJobConfig.java
@@ -2,7 +2,6 @@ package com.taejin.study.jobs;
 
 import com.taejin.study.domain.User;
 import com.taejin.study.domain.UserStatus;
-import com.taejin.study.jobs.readers.QueueItemReader;
 import com.taejin.study.repository.UserRepository;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -11,20 +10,30 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManagerFactory;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class InactiveUserJobConfig {
 
-    private final UserRepository userRepository;
+    private static final int CHUNK_SIZE = 15;
 
-    public InactiveUserJobConfig(UserRepository userRepository) {
+    private final UserRepository userRepository;
+    private final EntityManagerFactory entityManagerFactory;
+
+    public InactiveUserJobConfig(UserRepository userRepository, EntityManagerFactory entityManagerFactory) {
         this.userRepository = userRepository;
+        this.entityManagerFactory = entityManagerFactory;
     }
 
     /*
@@ -43,25 +52,53 @@ public class InactiveUserJobConfig {
     @Bean
     public Step inactiveJobStep(StepBuilderFactory stepBuilderFactory) {
         return stepBuilderFactory.get("inactiveUserStep")
-                .<User, User> chunk(5)
+                .<User, User> chunk(CHUNK_SIZE)
                 .reader(inactiveUserReader())
                 .processor(inactiveUserProcessor())
                 .writer(inactiveUserWriter())
                 .build();
     }
-
-//    @Bean
-//    @StepScope
-//    public QueueItemReader<User> inactiveUserReader() {
-//        List<User> oldUsers = userRepository.findByUpdatedAtBeforeAndStatusEquals(LocalDate.now().minusYears(1), UserStatus.ACTIVE);
-//        return new QueueItemReader<>(oldUsers);
-//    }
-
+    /*
+    *  한번에 모든 데이터를 읽어 메모리에 로딩한다. 데이터 건수가 많아질 수록 메모리 관련 이슈 발생
+    * */
     @Bean
     @StepScope
     public ListItemReader<User> inactiveUserReader() {
         List<User> oldUsers = userRepository.findByUpdatedAtBeforeAndStatusEquals(LocalDate.now().minusYears(1L), UserStatus.ACTIVE);
         return new ListItemReader<>(oldUsers);
+    }
+
+    /*
+    *  한번에 설정한 Paging Size 만큼, 데이터를 읽어온다. 대신, Reader를 통해 데이터를 읽을 때는 raw sql문을 통해서 만 읽어 올 수 있다.
+    * */
+    @Bean(destroyMethod = "")
+    @StepScope
+    public JpaPagingItemReader<User> inactiveUserJpaReader() {
+        JpaPagingItemReader<User> jpaPagingItemReader = new JpaPagingItemReader<>() {
+            @Override
+            public int getPage() {
+                return 0;
+            }
+        };
+        jpaPagingItemReader.setQueryString("select u from User as u where u.updatedAt < :updatedAt and u.status = :status");
+
+        Map<String, Object> map = new HashMap<>();
+        LocalDateTime now = LocalDateTime.now();
+        map.put("updatedAt", now.minusYears(1L));
+        map.put("status", UserStatus.ACTIVE);
+        jpaPagingItemReader.setParameterValues(map);
+        jpaPagingItemReader.setEntityManagerFactory(entityManagerFactory);
+        jpaPagingItemReader.setPageSize(CHUNK_SIZE);
+
+//        JpaPagingItemReader<User> userReader = new JpaPagingItemReaderBuilder<User>()
+//                .name("userReader")
+//                .entityManagerFactory(entityManagerFactory)
+//                .queryString("select u from User as u where u.updatedAt < :updatedAt and u.status = :status")
+//                .parameterValues(map)
+//                .pageSize(CHUNK_SIZE)
+//                .build();
+
+        return jpaPagingItemReader;
     }
 
     public ItemProcessor<User, User> inactiveUserProcessor() {
